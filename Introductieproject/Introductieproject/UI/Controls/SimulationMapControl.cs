1c2c86419ee2ee1ae74f4866f80c5c12de53901a
Quint Stoffers <quipeace@gmail.com>
2012-11-26 16:08:28 +0100

    Classes en namespaces aangemaak (stc)

---
+++ Introductieproject/Introductieproject/UI/Controls/SimulationMapControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Introductieproject.UI.Controls
+{
+    public partial class SimulationMapControl : UserControl
+    {
+        public SimulationMapControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

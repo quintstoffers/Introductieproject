dec8e0b579abc92055a66efa7f0d68d10f71a765
Vincent van der Zijden <vzijden@gmail.com>
2012-12-17 18:08:46 +0100

    reflection uitgezocht
    nu kunnen variabelen "programatically"(ik weet het nedelandse
    equivalent niet) automatisch geselecteerd worden uit een object aan de
    hand van de waarden van de uit de XML.

--- Introductieproject/Introductieproject/Simulation/parser.cs
+++ Introductieproject/Introductieproject/Simulation/parser.cs
@@ -4,17 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using Introductieproject.Objects;
+using System.Reflection;
 
 namespace Introductieproject.Simulation
 {
     class Parser
     {
         XmlDocument database = new XmlDocument();
-        Airplane airplane;
+        BO_747 B747 = new BO_747();
         List<Airplane> airplanes = new List<Airplane>();
+        
         public Parser()
         {
+          
             database.Load(@"Simulation\vliegtuigen.xml");
+
         }
 
         public string getallplanes()
@@ -38,22 +42,30 @@ namespace Introductieproject.Simulation
         public void update()
     {
         XmlNodeList planes = database.GetElementsByTagName("plane");
-        int amount = planes.Count;
-        for (int i = 0; i < amount; i++)
-        {   
-            int amountvar = planes.Item(i).ChildNodes.Count;
-            for (int x = 0; x < amountvar; x++)
-            {
-                airplanes.Add(airplane);
-                string variable = (planes.Item(i).ChildNodes.Item(x).Name);
-                string value = (planes.Item(i).ChildNodes.Item(x).InnerText);
-              
+        switch (planes.Item(0).SelectSingleNode("type").InnerText)
+        {
+            case "747": Console.WriteLine(747);
+                fill747();
+                break;
+            default: Console.WriteLine("unknown planetype");
+                break;
 
             
-            }
-        }
 
+        }
     }
+        private void fill747()
+        {
+             XmlNodeList planes = database.GetElementsByTagName("plane");
+             Type type = typeof(BO_747);
+             FieldInfo variable = type.GetField("taxiSpeed");
+             variable.SetValue(B747, 3);
+             Console.WriteLine(variable.GetValue(B747).ToString());
+             foreach (XmlNode plane in planes)
+             {
+
+             }
+        }
 
     }
 }
